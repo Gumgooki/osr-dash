851e08903273681cd224b046ef6aa33ebf10d9d7
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2023-07-18 20:39:20 -0500

    added some notes, and started a header

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import Header from '@/components/Header'
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -22,8 +25,7 @@ export default function Home() {
               className="dark:invert"
               width={100}
               height={24}
-              priority
-            />
+              priority />
           </a>
         </div>
       </div>
@@ -35,8 +37,7 @@ export default function Home() {
           alt="Next.js Logo"
           width={180}
           height={37}
-          priority
-        />
+          priority />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -108,6 +109,6 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
+    </main></>
   )
 }
---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header(){
+    return (
+        <div class="flex flex-row">
+            <div>
+                <h2>OSR Dashboard</h2>
+            </div>
+            <div>
+                <ul class="flex flex-row space-x-6">
+                    <li>Home</li>
+                    <li>Characters</li>
+                    <li>Campaigns</li>
+                    <li>Generators</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
